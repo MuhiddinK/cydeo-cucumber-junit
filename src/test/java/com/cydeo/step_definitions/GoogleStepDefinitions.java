fff35b1f4a8e95331b63c5b268af06e48afdb97e
MuhiddinK <muti67200@gmail.com>
2022-06-16 23:18:20 +0200

    cucumber project

---
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -0,0 +1,32 @@
+package com.cydeo.step_definitions;
+
+import com.cydeo.utilities.Driver;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class GoogleStepDefinitions {
+
+    @When("user is on Google search page")
+    public void user_is_on_google_search_page() {
+
+        Driver.getDriver().get("https://www.google.com");
+
+        //AgreeButton problem has been solved
+        WebElement agreeButton = Driver.getDriver().findElement(By.id("L2AGLb"));
+        agreeButton.click();
+
+    }
+    @Then("user should see title is Google")
+    public void user_shoould_see_title_is_google() {
+        String expectedTitle = "Google";
+        String actualTitle = Driver.getDriver().getTitle();
+
+        Assert.assertEquals(actualTitle, expectedTitle);
+
+        Driver.closeDriver();
+    }
+
+}
